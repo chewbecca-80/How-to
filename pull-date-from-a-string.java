623a353db48e32904fa87e81532f88b11522d317
chewbecca-80 <33635263+chewbecca-80@users.noreply.github.com>
2018-02-16 16:37:46 -0500

    Update pull-date-from-a-string.java

--- pull-date-from-a-string.java
+++ pull-date-from-a-string.java
@@ -1,52 +1,40 @@
 private String findMatchingRegexDate(String s) {
 		ArrayList<RegexMatcher> regexObjects = new ArrayList<RegexMatcher>() {
 			{
-
-				add(new RegexMatcher(".*[a-zA-Z]{4,}\\s\\d{1,2}\\s\\d{4}.*", "[a-zA-Z]{4,}\\s\\d{1,2}\\s\\d{4}",
-						"MMMM dd yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{4,}\\s\\d{1,2},\\s\\d{4}.*", "[a-zA-Z]{4,}\\s\\d{1,2},\\s\\d{4}",
-						"MMMM dd, yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{4,}\\s\\d{1,2},\\d{4}.*", "[a-zA-Z]{4,}\\s\\d{1,2},\\d{4}",
-						"MMMM dd,yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{4,}[.]\\s\\d{1,2}\\s\\d{4}.*", "[a-zA-Z]{4,}[.]\\s\\d{1,2}\\s\\d{4}",
-						"MMMM. dd yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{4,}[.]\\s\\d{1,2},\\s\\d{4}.*", "[a-zA-Z]{4,}[.]\\s\\d{1,2},\\s\\d{4}",
-						"MMMM. dd, yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{4,}[.]\\s\\d{1,2},\\d{4}.*", "[a-zA-Z]{4,}[.]\\s\\d{1,2},\\d{4}",
-						"MMMM. dd,yyyy"));
-				add(new RegexMatcher(".*\\d{1,2}\\s[a-zA-Z]{4,}\\s\\d{4}.*", "\\d{1,2}\\s[a-zA-Z]{4,}\\s\\d{4}",
-						"dd MMMM yyyy"));
-				add(new RegexMatcher(".*\\d{1,2}\\s[a-zA-Z]{4,}[.]\\s\\d{4}.*", "\\d{1,2}\\s[a-zA-Z]{4,}[.]\\s\\d{4}",
-						"dd MMMM. yyyy"));
-				add(new RegexMatcher(".*\\d{1,2}\\s[a-zA-Z]{3}\\s\\d{4}.*", "\\d{1,2}\\s[a-zA-Z]{3}[.]\\s\\d{4}",
-						"dd MMM. yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{3}\\s\\d{1,2},\\d{4}.*", "[a-zA-Z]{3}\\s\\d{1,2},\\d{4}",
-						"MMM dd,yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{4,}[.]\\s\\d{1,2}\\s\\d{4}.*", "[a-zA-Z]{4,}[.]\\s\\d{1,2}\\s\\d{4}",
-						"MMM. dd yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{3}\\s\\d{1,2},\\s\\d{4}.*", "[a-zA-Z]{3}\\s\\d{1,2}\\s,\\d{4}",
-						"MMM dd, yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{3}[.]\\s\\d{1,2},\\d{4}.*", "[a-zA-Z]{3}[.]\\s\\d{1,2},\\d{4}",
-						"MMM. dd,yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{3}[.]\\s\\d{1,2},\\s\\d{4}.*", "[a-zA-Z]{3}[.]\\s\\d{1,2},\\s\\d{4}",
-						"MMM. dd, yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{3}\\s\\d{1,2}\\s\\d{4}.*", "[a-zA-Z]{3}\\s\\d{1,2}\\s\\d{4}",
-						"MMM dd yyyy"));
-				add(new RegexMatcher(".*[a-zA-Z]{3}\\s\\d{1,2}[,]\\s\\d{4}.*", "[a-zA-Z]{3}\\s\\d{1,2}[,]\\s\\d{4}",
-						"MMM dd, yyyy"));
-				add(new RegexMatcher(".*\\d{4}/\\d{1,2}/\\d{1,2}.*", "\\d{4}/\\d{1,2}/\\d{1,2}", "yyyy/mm/dd"));
-				add(new RegexMatcher(".*\\d{1,2}/\\d{1,2}/\\d{4}.*", "\\d{1,2}/\\d{1,2}/\\d{4}", "mm/dd/yyyy"));
-				add(new RegexMatcher(".*\\d{1,2}-\\d{1,2}-\\d{4}.*", "[^-\\d]\\d{1,2}-\\d{1,2}-\\d{4}", "mm-dd-yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}\\s\\d{1,2}\\s\\d{4}", "MMMM dd yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}\\s\\d{1,2},\\s\\d{4}", "MMMM dd, yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}\\s\\d{1,2},\\d{4}", "MMMM dd,yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}[.]\\s\\d{1,2}\\s\\d{4}", "MMMM. dd yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}[.]\\s\\d{1,2},\\s\\d{4}", "MMMM. dd, yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}[.]\\s\\d{1,2},\\d{4}", "MMMM. dd,yyyy"));
+				add(new RegexMatcher("\\d{1,2}\\s[a-zA-Z]{4,}\\s\\d{4}", "dd MMMM yyyy"));
+				add(new RegexMatcher("\\d{1,2}\\s[a-zA-Z]{4,}[.]\\s\\d{4}", "dd MMMM. yyyy"));
+				add(new RegexMatcher("\\d{1,2}\\s[a-zA-Z]{3}[.]\\s\\d{4}", "dd MMM. yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{3}\\s\\d{1,2},\\d{4}", "MMM dd,yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{4,}[.]\\s\\d{1,2}\\s\\d{4}", "MMM. dd yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{3}\\s\\d{1,2}\\s,\\d{4}", "MMM dd, yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{3}[.]\\s\\d{1,2},\\d{4}", "MMM. dd,yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{3}[.]\\s\\d{1,2},\\s\\d{4}", "MMM. dd, yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{3}\\s\\d{1,2}\\s\\d{4}", "MMM dd yyyy"));
+				add(new RegexMatcher("[a-zA-Z]{3}\\s\\d{1,2}[,]\\s\\d{4}", "MMM dd, yyyy"));
+				add(new RegexMatcher("\\d{4}/\\d{1,2}/\\d{1,2}", "yyyy/mm/dd"));
+				add(new RegexMatcher("\\d{1,2}/\\d{1,2}/\\d{4}", "mm/dd/yyyy"));
+				add(new RegexMatcher("[^-\\d]\\d{1,2}-\\d{1,2}-\\d{4}", "mm-dd-yyyy"));
 				// Issue
-				add(new RegexMatcher(".*\\d{1,2}\\s[a-zA-Z]{3}\\s\\d{4}.*", "\\d{1,2}\\s[a-zA-Z]{3}\\s\\d{4}",
-						"dd MMM yyyy"));
-
-				add(new RegexMatcher(".*\\d{4}-\\d{1,2}-\\d{1,2}.*", "[^-\\d]\\d{4}-\\d{1,2}-\\d{1,2}", "yyyy-mm-dd"));
-
-				add(new RegexMatcher(".*\\d{4}\\s-\\s\\d{1,2}\\s-\\s\\d{1,2}.*", "[^-\\d]\\d{4}\\s-\\s\\d{1,2}\\s-\\s\\d{1,2}",
-						"yyyy - mm - dd"));
-				add(new RegexMatcher(".*\\d{8}.*", "\\d{8}", "yyyymmdd"));
-
+				add(new RegexMatcher("\\d{1,2}\\s[a-zA-Z]{3}\\s\\d{4}", "dd MMM yyyy"));
+				add(new RegexMatcher("[^-\\dA-Z-a-z]\\d{4}-\\d{1,2}-\\d{1,2}", "yyyy-mm-dd"));
+				add(new RegexMatcher("[^-\\d]\\d{4}\\s-\\s\\d{1,2}\\s-\\s\\d{1,2}", "yyyy - mm - dd"));
+				add(new RegexMatcher("20\\d{2}[0-1][0-9][0-3][0-9]", "yyyymmdd"));
+				add(new RegexMatcher("\\d{1,2}[-][A-Za-z]{3,}[-]\\d{4}", "dd-MMMM-yyyy"));
+				add(new RegexMatcher("[A-Za-z]{3,}[.]\\d{4}", "MMMM.yyyy"));
+				add(new RegexMatcher("\\d{1,2}\\s[A-Za-z]{3,}\\s{1,2}\\d{4}", "dd MMMM yyyy"));
+				add(new RegexMatcher("\\d{1,2}[-][A-Za-z]{3,}[-]\\d{2}", "dd-MMMM-yy"));
+				add(new RegexMatcher("[A-Za-z]{3,}[.]\\s\\d{1,2}\\s\\d{4}", "MMMM. dd yyyy"));
+				add(new RegexMatcher("[A-Za-z]{3,}[.]\\d{4}", "MMMM.yyyy"));
+				add(new RegexMatcher("[A-Za-z]{3,}[.]\\s\\d{4}", "MMMM. yyyy"));
+				add(new RegexMatcher("\\d{1,2}\\s[A-Za-z]{3,}\\s\\d{2}", "dd MMMM yy"));
+				add(new RegexMatcher("[^-\\dA-Za-z]\\d{1,2}[A-Za-z]{3,}\\d{2}", "ddMMMMyy"));
+				add(new RegexMatcher("[^-\\dA-Za-z]\\d{1,2}[a-zA-Z]{3,}\\d{4}", "ddMMMMyyyy"));
 			}
 		};
 
@@ -65,13 +53,11 @@ private String findMatchingRegexDate(String s) {
   
   public class RegexMatcher {
 
-	Pattern fullPattern;
 	Pattern shortPattern;
 	String patternStyle;
 	
 	
-	RegexMatcher(String fullPattern, String shortPattern, String style) {
-		this.fullPattern = Pattern.compile(fullPattern);
+	RegexMatcher(String shortPattern, String style) {		
 		this.shortPattern = Pattern.compile(shortPattern);
 		this.patternStyle = style;
 	}
