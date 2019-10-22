9375fca42e89c1b57e18c5b4f27412153545dd8f
chewbecca-80 <33635263+chewbecca-80@users.noreply.github.com>
2019-10-22 16:01:43 -0400

    Update read-a-file-python.py

--- read-a-file-python.py
+++ read-a-file-python.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 def read_file(file_path:Path):
-  with open(file_path) as f:
-    line = f.readline()
-    print(line)
+  with open(file_path) as f:    
+      line = f.readline()
+      while(line):
+        print(line)
+        line = f.readline()
